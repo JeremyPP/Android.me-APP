946e03e3b0c207200997919df303b68f8f4bca13
Mohammed Header <mohheader@hotmail.com>
2014-06-03 21:04:43 +0300

    #10 Up Button Acts as Back Button

--- App/AndroidMeFeed/src/com/ndroidme/ArticleActivity.java
+++ App/AndroidMeFeed/src/com/ndroidme/ArticleActivity.java
@@ -179,5 +179,16 @@ public class ArticleActivity extends Activity {
 		
 		return true;
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	        switch (item.getItemId()) {
+	        case android.R.id.home:
+	            onBackPressed();
+	            return true;
+	        }
+
+	    return super.onOptionsItemSelected(item);
+	}
 
 }
