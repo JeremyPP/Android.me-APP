a26b54735a1d7c53e0e9825f32915066ca8fe9e2
mikez23 <codingzebra@gmail.com>
2014-09-27 16:38:05 -0400

    updated universal-image-loader from version 1.9.2 to 1.9.3

--- App/AndroidMeFeed/src/com/ndroidme/NdroidApplication.java
+++ App/AndroidMeFeed/src/com/ndroidme/NdroidApplication.java
@@ -1,11 +1,10 @@
 package com.ndroidme;
 
+import android.app.Application;
+import com.nostra13.universalimageloader.core.*;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-
-import android.app.Application;
-
 public class NdroidApplication extends Application {
 
 	@Override
